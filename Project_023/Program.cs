0ecaa59e78ca761769907d6c8701bf9d98632405
Телегина Ирина <ikoska056@gmail.com>
2022-02-18 15:14:38 +0300

    Решение задачи 23

--- Project_023/Program.cs
+++ Project_023/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 23. Показать таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write($"Введите число:   ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+int[] GetSqrtTable(int N)
+{
+    int[] SqrtArray = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        SqrtArray[i] = number * number;
+        number++;
+    }
+    return SqrtArray;
+}
+
+void PrintTable(int[] Array)
+{
+    int number = 1;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        System.Console.Write($"{number} ^ 2 = ");
+        System.Console.WriteLine(Array[i]);
+        number++;
+    }
+}
+
+int[] SqrtTable = GetSqrtTable(N);
+PrintTable(SqrtTable);

1048f70c0ca88e151125deb7978478cbad674c88
Телегина Ирина <ikoska056@gmail.com>
2022-02-18 15:29:17 +0300

    Решение задачи 26

--- Project_026/Program.cs
+++ Project_026/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 10;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
